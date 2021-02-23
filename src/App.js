accf41b41e9f241684bfdf4e19701e70153246df
a-rfl <59601726+a-rfl@users.noreply.github.com>
2021-02-23 12:57:34 +0100

    Add general style properties

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
-import logo from './logo.svg';
 import './App.scss';
+import Footer from './components/footer/Footer';
+import Header from './components/header/Header';
+import SectionHome from './components/section1/SectionHome';
+import SectionArchives from './components/section2/SectionArchives';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <main className="App">
+      <Header imgName="logo.png" imgAlt="Logo du restaurant Maison Salvi" />
+      <SectionHome btnFunction={() => console.log('ok')}>
         <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
+          La Maison Salvi vous offre chaque jour le choix entre un menu carné ou
+          végé: 10&nbsp;&euro; livraison incluse (sur Rennes uniquement).
+          <br /> <br />
+          Des produits sains et simples, de saison, mis en musique et en couleur
+          par le chef Pablo. <br />
+          Qui d&apos;autre aurait pu réconcilier la famille autour d&apos;une
+          idée aussi simple et chaleureuse&nbsp;?<br /> <br />
+          Il est temps de passer commande !
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </SectionHome>
+      <SectionArchives btnFunction={() => console.log('ok')} />
+      <Footer />
+    </main>
   );
 }
 
