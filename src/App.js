641657007fe92b9025ec62667331260eba270fe3
a-rfl <59601726+a-rfl@users.noreply.github.com>
2021-02-23 15:28:27 +0100

    Update content modal with function to get the menu

--- src/App.js
+++ src/App.js
@@ -1,26 +1,81 @@
+import { useState } from 'react';
 import './App.scss';
 import Footer from './components/footer/Footer';
 import Header from './components/header/Header';
+import Modal from './components/Modal/Modal';
 import SectionHome from './components/section1/SectionHome';
 import SectionArchives from './components/section2/SectionArchives';
+// Data
+import menus from './data/menus';
 
 function App() {
+  // To get the date of the dat
+  const date = new Date();
+  // To get the month in french
+  const months = ['janvier', 'février', 'mars', 'avril', 'mai', 'juin', 'juillet', 'août', 'septembre', 'octobre', 'novembre', 'décembre'];
+  const actualDate = `${date.getDate()} ${months[date.getMonth()]}`;
+  /* -- MODAL -- */
+  // States Modal : Visibility & Content
+  const [visibility, setVisibility] = useState(false);
+  const [modalContent, setModalContent] = useState({
+    date: '',
+    entrees: [],
+    plats: [],
+    desserts: [],
+  });
+  // Click event function for show the menu of the day
+  const toggleModal = (e) => {
+    e.preventDefault();
+    // Display the modal
+    setVisibility(!visibility);
+    if (!visibility) {
+      // Find the menu depending on the actual date
+      const findMenuOfTheDay = (item) => item.date === actualDate;
+      const menuOfTheDay = menus.filter(findMenuOfTheDay);
+      // Each part of the menu of the day
+      const entreesOfTheDay = menuOfTheDay[0].entrees;
+      const platsOfTheDay = menuOfTheDay[0].plats;
+      const dessertsOfTheDay = menuOfTheDay[0].desserts;
+      setModalContent({
+        date: actualDate,
+        entrees: entreesOfTheDay,
+        plats: platsOfTheDay,
+        desserts: dessertsOfTheDay,
+      });
+    }
+  };
   return (
     <main className="App">
       <Header imgName="logo.png" imgAlt="Logo du restaurant Maison Salvi" />
-      <SectionHome btnFunction={() => console.log('ok')}>
-        <p>
-          La Maison Salvi vous offre chaque jour le choix entre un menu carné ou
-          végé: 10&nbsp;&euro; livraison incluse (sur Rennes uniquement).
-          <br /> <br />
-          Des produits sains et simples, de saison, mis en musique et en couleur
-          par le chef Pablo. <br />
-          Qui d&apos;autre aurait pu réconcilier la famille autour d&apos;une
-          idée aussi simple et chaleureuse&nbsp;?<br /> <br />
-          Il est temps de passer commande !
-        </p>
+      <SectionHome btnFunction={toggleModal}>
+        La Maison Salvi vous offre chaque jour le choix entre un menu carné ou
+        végé: 10&nbsp;&euro; livraison incluse (sur Rennes uniquement).
+        <br /> <br />
+        Des produits sains et simples, de saison, mis en musique et en couleur
+        par le chef Pablo. <br />
+        Qui d&apos;autre aurait pu réconcilier la famille autour d&apos;une
+        idée aussi simple et chaleureuse&nbsp;?<br /> <br />
+        Il est temps de passer commande !
       </SectionHome>
       <SectionArchives btnFunction={() => console.log('ok')} />
+      <Modal
+        date={modalContent.date}
+        btnFunction={toggleModal}
+        visibile={visibility}
+      >
+        <span>Entrées</span>
+        <ul>
+          {modalContent.entrees.map((item) => <li>{item}</li>)}
+        </ul>
+        <span>Plats</span>
+        <ul>
+          {modalContent.plats.map((item) => <li>{item}</li>)}
+        </ul>
+        <span>Desserts</span>
+        <ul>
+          {modalContent.desserts.map((item) => <li>{item}</li>)}
+        </ul>
+      </Modal>
       <Footer />
     </main>
   );
--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -2,33 +2,34 @@ import PropTypes from 'prop-types';
 import './_modal.scss';
 
 const Modal = ({
-  contentModal, btnFunction, visibile, contactNumber,
+  date, children, btnFunction, visibile, contactNumber,
 }) => (
   <div className={visibile ? 'visible' : 'invisible'}>
     <div className="modal-overlay">
       <div className="modal">
         <h2>menu du <span>jour</span></h2>
+        <h3>{date}</h3>
         <button className="btn-modal" type="button" onClick={btnFunction}>
           X
         </button>
         <p>
-          {contentModal}
+          {children}
         </p>
-        <p className="command-text">Pour commander, contacter le chef Pablo par Whatsapp : <span>{contactNumber}</span></p>
+        <p className="command-text">Pour commander via Whatsapp : <span>{contactNumber}</span></p>
       </div>
     </div>
   </div>
 );
 
 Modal.propTypes = {
-  contentModal: PropTypes.string,
+  date: PropTypes.string.isRequired,
+  children: PropTypes.string.isRequired,
   btnFunction: PropTypes.func.isRequired,
   visibile: PropTypes.bool,
   contactNumber: PropTypes.string,
 };
 
 Modal.defaultProps = {
-  contentModal: 'Ceci est le texte d\'une modale. ',
   visibile: false,
   contactNumber: '06 66 666 666',
 };
